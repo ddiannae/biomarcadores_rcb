3d0bfae880aaaa7a92fbfa28a224d450f0e591ec
Diana García <dgarcia@inmegen.edu.mx>
2022-04-05 22:57:45 -0500

    Agregando distribuciones conjuntas de genes

--- qPCR/qPCR.R
+++ qPCR/qPCR.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(ggplot2)
 library(ggpubr)
 library(janitor)
+library(tidyr)
 
 pacientes <- read_excel("../data/Base de datos-Incanet-Patología-INCAN .xlsx", 
                         skip = 2) %>% clean_names()
@@ -64,3 +65,56 @@ lapply(genes, function(gen) {
   print(gp)
   dev.off()
 })
+
+
+combns <- expand_grid(gen1 = genes, gen2 = genes) %>% 
+  filter(gen1 != gen2) %>% 
+  mutate(name = paste0(pmin(gen1, gen2), "-", pmax(gen1, gen2))) %>%
+  distinct(name, .keep_all = T) %>% select (-name)
+
+all_dfs <- mapply(function(gen1, gen2) {
+  
+  df_qpcr <- qpcr %>% select(expr = all_of(gen1), resistance) %>%
+    mutate(gen = toupper(gen1)) %>%
+    bind_rows(qpcr %>% select(expr = all_of(gen2), resistance) %>%
+                mutate(gen = toupper(gen2)))
+
+  gp <- ggplot(df_qpcr, aes(x = resistance, y = expr)) +
+    geom_violin(color="grey", size = 2, show.legend = F) +
+    stat_summary(fun = "median", geom = "crossbar", width = 0.5, show.legend = F,) +
+    geom_dotplot(aes(fill = gen), binaxis= "y",
+                 stackdir = "center", dotsize = 1) +
+    ylab("qPCR expression") +
+    xlab("") +
+    ggtitle(paste0(toupper(gen1), "-", toupper(gen2), " expression")) +
+    theme_bw(base_size = 30) +
+    stat_compare_means(method = "wilcox.test", show.legend = F,  label.y.npc = "top",
+                       label.x.npc = "center") +
+    scale_x_discrete(labels=c("Sensitive","Resistant")) +
+    scale_color_brewer(palette = "Set2", name = "", labels = c("Sensitive", "Resistant")) +
+    scale_fill_brewer(palette = "Pastel1", name = "")
+  
+  png(filename = paste0("plots_qPCR/", toupper(gen1), "_", toupper(gen2),  ".png"), 
+      width = 800, height = 600)
+  print(gp)
+  dev.off()
+  
+  gp <- ggplot(df_qpcr, aes(x = resistance, y = expr)) +
+    geom_boxplot(color="grey", size = 2, show.legend = F) +
+    geom_dotplot(aes(fill = gen), binaxis= "y",
+                 stackdir = "center", dotsize = 1) +
+    ylab("qPCR expression") +
+    xlab("") +
+    ggtitle(paste0(toupper(gen1), "-", toupper(gen2), " expression")) +
+    theme_bw(base_size = 30) +
+    stat_compare_means(method = "wilcox.test", show.legend = F,  label.y.npc = "top",
+                       label.x.npc = "center") +
+    scale_x_discrete(labels=c("Sensitive","Resistant")) +
+    scale_color_brewer(palette = "Set2", name = "", labels = c("Sensitive", "Resistant")) +
+    scale_fill_brewer(palette = "Pastel1", name = "")
+  
+  png(filename = paste0("plots_qPCR/", toupper(gen1), "_", toupper(gen2),  "_boxplot.png"), 
+      width = 800, height = 600)
+  print(gp)
+  dev.off()
+}, combns$gen1, combns$gen2, SIMPLIFY = FALSE)
